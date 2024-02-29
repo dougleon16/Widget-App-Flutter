61a4ab6280bfa9dbec54a078c16adc68050f15ac
Douglas leon <leondouglas1516@gmail.com>
2024-02-29 15:16:48 -0500

    Somo variable names was changed

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart' show IconData, Icons;
 
 class MenuItems {
-  final String tittle;
+  final String title;
   final String subtitle;
   final String link;
   final IconData icon;
 
   const MenuItems(
-      {required this.tittle,
+      {required this.title,
       required this.subtitle,
       required this.link,
       required this.icon});
@@ -15,12 +15,12 @@ class MenuItems {
 
 const appMenuItems = <MenuItems>[
   MenuItems(
-      tittle: 'Botones',
+      title: 'Botones',
       subtitle: 'Varios Botones en Flutter',
       link: '/buttons',
       icon: Icons.smart_button),
   MenuItems(
-      tittle: 'Tarjetas',
+      title: 'Tarjetas',
       subtitle: 'Un contenedor estilizado',
       link: '/card',
       icon: Icons.credit_card),
