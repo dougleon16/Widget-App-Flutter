dfba43907799a8561d4a93b659cfd7038e4c4731
Douglas leon <leondouglas1516@gmail.com>
2024-02-29 13:55:34 -0500

    AppBar Styles Modified

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.green,
+  Colors.purple,
+  Colors.orange,
+  Colors.black,
+  Colors.yellow,
+  Colors.pink,
+  Colors.brown,
+  Colors.teal
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0,
+            'selectedColor must be between 0 and ${colorList.length - 1}'),
+        assert(selectedColor < colorList.length,
+            'Color must be between 0 and ${colorList.length - 1}');
+
+  ThemeData gettheme() => ThemeData(
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: false));
+}
