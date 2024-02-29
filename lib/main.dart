aa9cc1e4680ea98fc35468b4293f79de0822454e
Douglas leon <leondouglas1516@gmail.com>
2024-02-29 13:56:23 -0500

    HomeScreen Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        theme: AppTheme(selectedColor: 5).gettheme(),
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen());
   }
 }
